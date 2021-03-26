22ba8e2e142b68ae4607311938118b783018e377
onurinc <onur.i@hotmail.nl>
2021-03-26 16:53:03 +0100

    Added solution for PEuler 7, 8 and 10

---
+++ PEuler10 Summation of primes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace PEuler10_Summation_of_primes
+{
+    class Program
+    {
+        static bool isPrime(int value)
+        {
+            for (int i = 2; i <= Math.Sqrt(value); i++)
+            {
+                if (value%i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> primeList = new List<int>();
+            for (int i = 2; i < 2000000; i++)
+            {
+                if (isPrime(i))
+                {
+                    primeList.Add(i);
+                }
+            }
+
+            long summation = 0;
+            for (int i = 0; i < primeList.Count; i++)
+            {
+                summation += primeList[i];
+            }
+
+            Console.WriteLine(summation);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PEuler7 100001st prime/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace PEuler8_100001st_prime
+{
+    class Program
+    {
+        static bool isPrime(int primeControl)
+        {
+            for (int i = 2; i <= Math.Sqrt(primeControl); i++)
+            {
+                if (primeControl % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            int number = 10001;
+            int primeControl = 2;
+            List<int> primeList = new List<int>();
+
+            while (primeList.Count != 10001)
+            {
+                if (isPrime(primeControl) == true)
+                {
+                    primeList.Add(primeControl);
+                }
+                primeControl++;
+            }
+            // primeList.ForEach(i => Console.Write("{0}\t", i));
+            Console.WriteLine(primeList[number-1]);
+            Console.ReadKey();
+        }
+    }
+}
